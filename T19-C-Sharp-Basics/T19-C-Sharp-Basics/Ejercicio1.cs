6d34d8ffa485cd1b72e804cb98a9537b0e6da4ec
santiarroyave <santi98cv@hotmail.com>
2023-08-09 23:47:35 +0200

    Ejercicio 1

---
+++ T19-C-Sharp-Basics/T19-C-Sharp-Basics/Ejercicio1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T19_C_Sharp_Basics
+{
+    internal class Ejercicio1
+    {
+        //ATRIBUTOS
+        int num1 = 15;
+        int num2 = 6;
+
+
+        public void Info()
+        {
+            Console.WriteLine("Valor 1: {0}", num1);
+            Console.WriteLine("Valor 2: {0}", num2);
+        }
+
+        public void Suma()
+        {
+            Console.WriteLine("Suma: {0}", num1 + num2);
+        }
+
+        public void Resta()
+        {
+            Console.WriteLine("Resta: {0}", num1 - num2);
+        }
+
+        public void Multiplicacion()
+        {
+            Console.WriteLine("Multiplicación: {0}", num1 * num2);
+        }
+
+        public void Division()
+        {
+            Console.WriteLine("División: {0}", num1 / num2);
+        }
+
+        public void Modulo()
+        {
+            Console.WriteLine("Modulo: {0}", num1 % num2);
+        }
+    }
+}
---
+++ T19-C-Sharp-Basics/T19-C-Sharp-Basics/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T19_C_Sharp_Basics
+{
+    class programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Iniciando Ejercicio 1");
+            Console.WriteLine("---------------------------------------------");
+            //Ejercicio1 1
+            Ejercicio1 ejercicio1 = new Ejercicio1();
+            ejercicio1.Info();
+            Console.WriteLine("----------------");
+
+            ejercicio1.Suma();
+            ejercicio1.Resta();
+            ejercicio1.Multiplicacion();
+            ejercicio1.Division();
+            ejercicio1.Modulo();
+
+            Console.ReadLine();
+
+
+        }
+    }
+    
+}
